6fd02a6b683bb5c697fdf85a433d322711b398a0
Diego Francisco <diego.francisco@fatec.sp.gov.br>
2019-01-21 16:04:42 -0200

    Feito a criação de um diretorio graphcool e o deploy
    Signed-off-by: Diego Francisco <diego.francisco@fatec.sp.gov.br>

--- deliasfrancisco-project/graphcool/src/hello.js
+++ deliasfrancisco-project/graphcool/src/hello.js
@@ -1,4 +1,5 @@
-export default async event => {
+export default async;
+ event => {
   // you can use ES7 with async/await and even TypeScript in your functions :)
 
   await new Promise(r => setTimeout(r, 50))
