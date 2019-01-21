e57b81414d05697d034ae9d118daf84e62954150
Diego Francisco <diego.francisco@fatec.sp.gov.br>
2019-01-21 15:15:37 -0200

    Instalação do Graphcool CLI e adicionando mat-list
    Signed-off-by: Diego Francisco <diego.francisco@fatec.sp.gov.br>

--- deliasfrancisco-project/src/app/app.module.ts
+++ deliasfrancisco-project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule, MatListModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,7 +11,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
